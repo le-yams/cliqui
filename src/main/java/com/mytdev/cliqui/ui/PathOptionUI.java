a067a9f1598133bc44e71921c116ad6c61d92278
le-yams <ydisanto@gmail.com>
2014-04-25 10:20:16 +0200

    move jfilechooser selection mode reference from path selection mode constraint to path option ui

--- src/main/java/com/mytdev/cliqui/ui/PathOptionUI.java
+++ src/main/java/com/mytdev/cliqui/ui/PathOptionUI.java
@@ -2,12 +2,15 @@ package com.mytdev.cliqui.ui;
 
 import com.mytdev.cliqui.ui.spi.AbstractOptionUI;
 import com.mytdev.cliqui.beans.Option;
+import com.mytdev.cliqui.beans.PathSelectionMode;
 import com.mytdev.cliqui.beans.PathSelectionModeConstraint;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.EnumMap;
 import java.util.List;
+import java.util.Map;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFileChooser;
@@ -20,12 +23,20 @@ import javax.swing.JTextField;
  */
 public final class PathOptionUI extends AbstractOptionUI implements ActionListener {
 
+    private static final Map<PathSelectionMode, Integer> SELECTION_MODES = new EnumMap<>(PathSelectionMode.class);
+
+    static {
+        SELECTION_MODES.put(PathSelectionMode.FILES_ONLY, JFileChooser.FILES_ONLY);
+        SELECTION_MODES.put(PathSelectionMode.FILES_AND_DIRECTORIES, JFileChooser.FILES_AND_DIRECTORIES);
+        SELECTION_MODES.put(PathSelectionMode.DIRECTORIES_ONLY, JFileChooser.DIRECTORIES_ONLY);
+    }
+
     private final JLabel label = new JLabel();
 
     private final JTextField field = new JTextField();
-    
+
     private final JButton browseButton = new JButton("Browse...");
-    
+
     private final JFileChooser fileChooser = new JFileChooser();
 
     public PathOptionUI(Option option) {
@@ -37,14 +48,14 @@ public final class PathOptionUI extends AbstractOptionUI implements ActionListen
         fileChooser.setMultiSelectionEnabled(false);
         final PathSelectionModeConstraint selectionModeConstraint = option.getConstraint(PathSelectionModeConstraint.class);
         fileChooser.setFileSelectionMode(selectionModeConstraint != null
-            ? selectionModeConstraint.getMode().getValue() 
+            ? SELECTION_MODES.get(selectionModeConstraint.getMode())
             : JFileChooser.FILES_AND_DIRECTORIES);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         fileChooser.setSelectedFile(Paths.get(field.getText()).toFile());
-        if(fileChooser.showDialog(browseButton, "Select") == JFileChooser.APPROVE_OPTION) {
+        if (fileChooser.showDialog(browseButton, "Select") == JFileChooser.APPROVE_OPTION) {
             field.setText(fileChooser.getSelectedFile().toPath().toString());
         }
     }
@@ -53,7 +64,7 @@ public final class PathOptionUI extends AbstractOptionUI implements ActionListen
     public List<String> getCommandLineValue() {
         final List<String> cli = new ArrayList<>();
         final String path = field.getText();
-        if(path.isEmpty() == false) {
+        if (path.isEmpty() == false) {
             cli.add(getOption().getName());
             cli.add(path);
         }
